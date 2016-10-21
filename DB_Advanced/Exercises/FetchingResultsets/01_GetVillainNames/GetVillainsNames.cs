66978f2628f301ea1a659335f873f7aa2a12a1fe
aanguelov <aanguelov@gmail.com>
2016-10-21 14:42:20 +0300

    Add first two problems

---
+++ DB_Advanced/Exercises/FetchingResultsets/01_GetVillainNames/GetVillainsNames.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.SqlClient;
+
+class GetVillainsNames
+{
+    static void Main()
+    {
+        string connectionString = "Server=.; Database=MinionsDB; Trusted_Connection=True;";
+        SqlConnection connection = new SqlConnection(connectionString);
+        connection.Open();
+
+        using (connection)
+        {
+            string villansAndTheirMinions = "SELECT v.VillainName, Count(vm.MinionId) AS NumberOfMinions " +
+                                              "FROM Villains AS v " +
+                                             "INNER JOIN [dbo].[VillainsMinions] AS vm " +
+                                                "ON v.VillainId = vm.VillainId " +
+                                             "GROUP BY v.VillainName " +
+                                             "ORDER BY Count(vm.MinionId) DESC";
+            SqlCommand command = new SqlCommand(villansAndTheirMinions, connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            while(reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.Write(reader[i] + " "); 
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ DB_Advanced/Exercises/FetchingResultsets/02_GetMinionNames/GetMinionNames.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Text;
+
+class GetMinionNames
+{
+    static void Main()
+    {
+        int villainId = int.Parse(Console.ReadLine());
+        string connectionString = "Server=.; Database=MinionsDB; Trusted_Connection=True;";
+        SqlConnection connection = new SqlConnection(connectionString);
+        connection.Open();
+
+        using (connection)
+        {
+            string selectVillainWithMinions = "SELECT v.VillainName, m.Name, m.Age " +
+                                                "FROM Minions AS m " +
+                                               "INNER JOIN VillainsMinions AS vm " +
+                                                  "ON m.MinionId = vm.MinionId " +
+                                               "RIGHT JOIN Villains AS v " +
+                                                  "ON v.VillainId = vm.VillainId " +
+                                               "WHERE v.VillainId = @villainId";
+            SqlCommand command = new SqlCommand(selectVillainWithMinions, connection);
+            command.Parameters.AddWithValue("@villainId", villainId);
+            SqlDataReader reader = command.ExecuteReader();
+
+            StringBuilder result = new StringBuilder();
+            int minionsCount = 1;
+
+            reader.Read();
+
+            if (reader.HasRows)
+            {
+                result.AppendLine($"Villain: {reader[0]}");
+                if(reader[1].ToString() != "")
+                {
+                    result.AppendLine($"{minionsCount}. {reader[1]} {reader[2]}");
+
+                    while (reader.Read())
+                    {
+                        minionsCount++;
+                        result.AppendLine($"{minionsCount}. {reader[1]} {reader[2]}");
+                    }
+                }
+                else
+                {
+                    result.AppendLine("<no minions>");
+                }
+                
+
+                Console.WriteLine(result.ToString().TrimEnd());
+            }
+            else
+            {
+                Console.WriteLine($"No villain with ID {villainId} exists in the database.");
+            }
+
+        }
+    }
+}
