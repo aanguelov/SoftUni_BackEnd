2a8ea9c5a3d72d10028cb90874dc1050ec1421e6
aanguelov <aanguelov@gmail.com>
2016-11-16 12:46:10 +0200

    'Exam(August2015)–Media' done

---
+++ DB_Advanced/ExamPreparation/Exam(August2015)–Media/Top10FavouriteMovies/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Top10FavouriteMovies
+{
+    using Movies.Data;
+    using Movies.Models;
+    using Newtonsoft.Json;
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class Program
+    {
+        static void Main()
+        {
+            var ctx = new MoviesEntities();
+            var topFavouritedMovies = ctx.Movies
+                .Where(m => m.Restriction == AgeRestriction.Teen)
+                .Select(m => new
+                {
+                    isbn = m.Isbn,
+                    title = m.Title,
+                    favouritedBy = m.Users.Select(u => u.Username)
+                })
+                .OrderByDescending(m => m.favouritedBy.Count())
+                .ThenBy(m => m.title)
+                .Take(10);
+
+            var jsonMovies = JsonConvert.SerializeObject(topFavouritedMovies, Formatting.Indented);
+            File.WriteAllText("../../../exported/top-10-favourite-movies.json", jsonMovies);
+        }
+    }
+}
